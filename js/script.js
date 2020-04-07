f15dd2ea8a977bb7f244f7207bdcd7ee7a6672d4
shellymcs <61569144+shellymcs@users.noreply.github.com>
2020-04-07 13:09:08 +1200

    upload files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready( function(){
+let images = document.getElementsByTagName('a');
+$('.search').on('keyup', function(){
+    let search = $('.search').val().toLowerCase();
+    for (var i =0; i< images.length; i++) {
+        let searchVal = images[i].getAttribute('title');
+    
+    if (searchVal.toLowerCase().indexOf(search) > -1){
+    images[i].style.display ="";
+}else {
+    images[i].style.display = "none";
+}
+    }
+});
+});
+
+
+
+baguetteBox.run('.baguetteBoxOne','.main-gallery', {
+captions: true,
+buttons: true
+});
